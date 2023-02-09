45a8848f577c1d8ba683b1558e201a1bda9399b4
buggiman6969 <abdullaapon12@gmail.com>
2023-02-09 17:17:25 +0600

    dddd

--- math.js
+++ math.js
@@ -1,26 +1,28 @@
 // math pow
-const result = Math.pow(4,3);
+const result = Math.pow(4, 3);
 console.log(result);
 
 // math abs
 const math1 = 45;
 const math2 = 85;
 
-const gap = Math.abs (math1 - math2);
+const gap = Math.abs(math1 - math2);
 
-if( gap < 5){
+if (gap < 5) {
     console.log("bondhu hou");
 }
-else{
+else {
     console.log("dure soro");
 }
 
 // . to absolute number
 const number = 265.65454;
-const fullnum = Math.round (number);
+const fullnum = Math.round(number);
 console.log(fullnum);
 
 // random number
 
-const random =  Math.round(Math.random()*6);
-console.log(random);
+for (let i = 0; i < 6; i++) {
+    const random = Math.round(Math.random() * 6);
+    console.log(random);
+}
